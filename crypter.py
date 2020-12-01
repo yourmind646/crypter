2710394b6964209b29b7f13b83b640b3a88e9e86
yourmind646 <66122473+yourmind646@users.noreply.github.com>
2020-12-01 17:02:54 +0300

    Version 1.1
    Added exceptions

--- crypter.py
+++ crypter.py
@@ -1,45 +1,54 @@
 #!/usr/bin/env python3
-import os
-import colorama
-from colorama import Fore, Back, Style
+try:
+	import os
+	import colorama
+	from colorama import Fore, Back, Style
 
-colorama.init()
+	colorama.init()
 
-def crypt(file, password):
-	import pyAesCrypt
-	bufferSize = 512 * 1024
-	pyAesCrypt.encryptFile(str(file), str(file) + ".aes", password, bufferSize)
-	os.remove(file)
-	print('{}Файл {} успешно зафифрован!{}'.format(Fore.GREEN, file + ".aes", Fore.WHITE))
+	def crypt(file):
+		import pyAesCrypt
+		key = input('Введите ключ шифрования\n>>> ')
+		bufferSize = 512 * 1024
+		pyAesCrypt.encryptFile(str(file), str(file) + ".aes", key, bufferSize)
+		os.remove(file)
+		print('{}Файл {} успешно зашифрован!{}'.format(Fore.GREEN, file + ".aes", Fore.WHITE))
 
-def decrypt(file, password):
-	import pyAesCrypt
-	bufferSize = 512 * 1024
-	pyAesCrypt.decryptFile(str(file), str(os.path.splitext(file)[0]), password, bufferSize)
-	os.remove(file)
-	print('{}Файл {} успешно расшифрован!{}'.format(Fore.GREEN, file, Fore.WHITE))
+	def decrypt(file):
+		import pyAesCrypt
+		key = input('Введите ключ шифрования\n>>> ')
+		bufferSize = 512 * 1024
+		pyAesCrypt.decryptFile(str(file), str(os.path.splitext(file)[0]), key, bufferSize)
+		os.remove(file)
+		print('\n{}Файл {} успешно расшифрован!{}'.format(Fore.GREEN, file, Fore.WHITE))
 
-print(Fore.CYAN + '''
-╔═══╗────────╔╗───────╔╗─╔═══╗
-║╔═╗║───────╔╝╚╗─────╔╝║─║╔═╗║
-║║─╚╬═╦╗─╔╦═╩╗╔╬══╦═╗╚╗║─║║║║║
-║║─╔╣╔╣║─║║╔╗║║║║═╣╔╝─║║─║║║║║
-║╚═╝║║║╚═╝║╚╝║╚╣║═╣║─╔╝╚╦╣╚═╝║
-╚═══╩╝╚═╗╔╣╔═╩═╩══╩╝─╚══╩╩═══╝
+	print(Fore.CYAN + '''
+╔═══╗────────╔╗───────╔╗──╔╗
+║╔═╗║───────╔╝╚╗─────╔╝║─╔╝║
+║║─╚╬═╦╗─╔╦═╩╗╔╬══╦═╗╚╗║─╚╗║
+║║─╔╣╔╣║─║║╔╗║║║║═╣╔╝─║║──║║
+║╚═╝║║║╚═╝║╚╝║╚╣║═╣║─╔╝╚╦╦╝╚╗
+╚═══╩╝╚═╗╔╣╔═╩═╩══╩╝─╚══╩╩══╝
 ──────╔═╝║║║ Coded by TG @RubySide
 ──────╚══╝╚╝
-Создано для систем linux/termux.
-При применении данного скрипта, размер файла не должен превышать объем оперативной памяти.''' + Fore.WHITE)
+Создано для систем Linux/Termux.''' + Fore.WHITE)
 
-choose = input('\nВыберите действие: c/d (зашифровать/дешифровать): ')
+	choose = input('\nВыберите действие: c/d (зашифровать/дешифровать)\n>>> ')
 
-if choose == 'c':
-	path = input('Введите полный путь до шифруемого файла (пример: /sdcard/file.txt): ')
-	key = input('Введите ключ шифрования: ')
-	crypt(path, key)
-elif choose == 'd':
-	path = input('Введите полный путь до дешифруемого файла (пример: /sdcard/file.txt): ')
-	key = input('Введите ключ шифрования: ')
-	decrypt(path, key)
-else:
-	print('\n{}Ошибка!{}'.format(Fore.RED, Fore.WHITE))
+	if choose == 'c':
+		path = input('Введите путь до шифруемого файла (пример: /sdcard/file.txt)\n>>> ')
+		crypt(path)
+	elif choose == 'd':
+		path = input('Введите полный путь до дешифруемого файла (пример: /sdcard/file.txt)\n>>> ')
+		decrypt(path)
+	else:
+		print('\n{}Ошибка! Такого действия не существует.{}'.format(Fore.RED, Fore.WHITE))
+
+except EOFError:
+	print('\n{}Выход из программы...{}'.format(Fore.YELLOW, Fore.WHITE))
+except KeyboardInterrupt:
+	print('\n\n{}Выход из программы...{}'.format(Fore.YELLOW, Fore.WHITE))
+except OSError:
+	print('\n{}Ошибка! Файл не найден.{}'.format(Fore.RED, Fore.WHITE))
+except ValueError:
+	print('\n{}Ошибка! Введен неверный ключ или файл не зашифрован.{}'.format(Fore.RED, Fore.WHITE))
